a8f047c49a03cc8ae6a60c501e43ce7de25079a5
Michael Wong <mwckkwork@gmail.com>
2022-03-05 13:49:35 +0000

    improved test case

--- TriangleTest/src/test/java/com/example/TriangleTest.java
+++ TriangleTest/src/test/java/com/example/TriangleTest.java
@@ -16,4 +16,42 @@ public class TriangleTest
         Triangle t = new Triangle(3, 3, 3);
         assertEquals("Equilateral", t.getTriangleType());
     }    
+
+    @Test
+    public void testIsosceles() {
+        Triangle t = new Triangle(3, 3, 1);
+        assertEquals("Isosceles", t.getTriangleType());
+
+        Triangle t2 = new Triangle(1, 3, 3);
+        assertEquals("Isosceles", t2.getTriangleType());
+
+        Triangle t3 = new Triangle(3, 1, 3);
+        assertEquals("Isosceles", t3.getTriangleType());
+    }
+
+    @Test
+    public void testInvalideInput() {
+        String expectedResult = "Error: The length should be positive";
+        Triangle t = new Triangle(-1, 3, 3);
+        assertEquals(expectedResult, t.getTriangleType());
+        
+        Triangle t2 = new Triangle(3, -1, 3);
+        assertEquals(expectedResult, t2.getTriangleType());
+        
+        Triangle t3 = new Triangle(3, 3, -1);
+        assertEquals(expectedResult, t3.getTriangleType());
+    }  
+    
+    @Test
+    public void testNotTriangle() {
+        String expectedResult = "Error: Not a triangle";
+        Triangle t = new Triangle(6, 3, 3);
+        assertEquals(expectedResult, t.getTriangleType());
+        
+        Triangle t2 = new Triangle(3, 6, 3);
+        assertEquals(expectedResult, t2.getTriangleType());
+        
+        Triangle t3 = new Triangle(3, 3, 6);
+        assertEquals(expectedResult, t3.getTriangleType());
+    } 
 }
