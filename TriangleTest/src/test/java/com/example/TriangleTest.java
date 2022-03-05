df3a60dfdf8ff21f36ed0b09d6f7674ddaa4e956
Michael Wong <mwckkwork@gmail.com>
2022-03-05 13:06:06 +0000

    Re-created TriangleTest project

---
+++ TriangleTest/src/test/java/com/example/TriangleTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TriangleTest 
+{
+    @Test
+    public void testScalene() {
+        Triangle t = new Triangle(3, 4, 5);
+        assertEquals("Scalene", t.getTriangleType());
+    }
+
+    @Test
+    public void testEquilateral() {
+        Triangle t = new Triangle(3, 3, 3);
+        assertEquals("Equilateral", t.getTriangleType());
+    }    
+}
